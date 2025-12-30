c633ddb28490afdb3b7bcb9ea28d1dae5d3f7096
Lahama09 <nilanjanalahama@gmail.com>
2025-12-30 10:48:36 +0530

    Add modified app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 
 # Load model + vectorizer
 with open("model/model.pkl", "rb") as f:
